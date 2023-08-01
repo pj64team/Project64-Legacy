7c2549f6aad1f3d5d9eb44f7b8dadf82bcca8124
Jay Oster <jay@kodewerx.org>
2023-08-01 13:16:19 -0700

    Add TLB Miss exceptions to interpreter (#56)
    - Only some load instructions raised the TLB miss exception. Now they
      all do! And all store instructions, as well.
    - Doesn't add TLB Miss to the recompiler (looks like only stores are
      missing there).
    - Fixes Re-Volt! Gauntlet Legends now shows garbage on the screen
      instead of nothing.

--- Interpreter CPU.c
+++ Interpreter CPU.c
@@ -677,7 +677,7 @@ void BuildInterpreter (void ) {
 
 void ExecuteInterpreterOpCode (void) {
 	if (!r4300i_LW_VAddr_NonCPU(PROGRAM_COUNTER, &Opcode.Hex)) {
-		DoTLBMiss(NextInstruction == JUMP, PROGRAM_COUNTER);
+		DoTLBMiss(NextInstruction == JUMP, PROGRAM_COUNTER, TRUE);
 		NextInstruction = NORMAL;
 		return;
 	}
