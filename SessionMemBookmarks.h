654b8f09b13f133ff880fb93d9016f53d4e820d7
Jay Oster <jay@kodewerx.org>
2023-08-06 22:08:18 -0700

    Memory editor (#24)
    * Switch hex editor to full CUSTOMDRAW
    
    - This will allow us to draw with more precision. Especially for
      selection ranges and editing.
    
    * Add basic hex selection
    
    * Add ASCII column text selection
    
    * Ctrl+C to copy selected memory
    
    * Fix moving the window
    
    * Fix minor bugs in memory viewer with watchpoints
    
    - The colors and underlines were only showing when a watchpoint was
      disabled.
    - If the last hex byte in a row was underlined, the entire ASCII section
      would also be underlined on that row.
    - Now colors always show when a watchpoint exists. And the byte is
      underlined only when the watchpoint is enabled.
    
    * Fix copying the last line of memory with some selection sizes
    
    - This bug prevented the last line of the memory view from being copied
      to the clipboard when the start of the selection was at a higher
      column number than the end of the selection.
    - E.g. when address range 0x8000000c-0x80000023 was selected, it would
      only copy 0x8000000c-0x8000001f, truncating the last line and missing
      four selected bytes.
    
    * Add memory editor bookmarks
    
    * Add missing session files to VS project filters

---
+++ SessionMemBookmarks.h
@@ -0,0 +1,40 @@
+/*
+ * Project 64 - A Nintendo 64 emulator.
+ *
+ * (c) Copyright 2023 parasyte (jay@kodewerx.org)
+ *
+ * pj64 homepage: www.pj64.net
+ *
+ * Permission to use, copy, modify and distribute Project64 in both binary and
+ * source form, for non-commercial purposes, is hereby granted without fee,
+ * providing that this license information and copyright notice appear with
+ * all copies and any derived work.
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event shall the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Project64 is freeware for PERSONAL USE only. Commercial users should
+ * seek permission of the copyright holders first. Commercial use includes
+ * charging money for Project64 or software derived from Project64.
+ *
+ * The copyright holders request that bug fixes and improvements to the code
+ * should be forwarded to them so if they want them.
+ *
+ */
+
+#include <windows.h>
+
+#pragma once
+
+#define MAX_MEM_BOOKMARKS 1000
+
+struct MEM_BOOKMARK {
+	char name[100];
+	unsigned int width_required;
+	DWORD selection_range[2];
+	BOOL is_virtual;
+};
+
+BOOL Session_Load_MemBookmarks(unsigned int *num_bookmarks, struct MEM_BOOKMARK *bookmarks, unsigned int max_bookmarks, char *filename);
+BOOL Session_Save_MemBookmarks(char *filename, struct MEM_BOOKMARK *bookmarks, unsigned int num_bookmarks);
